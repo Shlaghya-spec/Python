74e9dd3ddc768bb55c1fc694d1a4a938a29e52b7
Shlaghya Bhosale <shlaghyabhosale@gmail.com>
2025-09-16 18:15:12 +0530

    Ch-1 variables

--- operators.py
+++ operators.py
@@ -5,54 +5,55 @@ print("Operators")
 a = 5
 b = 7
 
-# print("the Arithmetic Operators are")
-# print(a + b) #addition
-# print(a - b) #sub
-# print(a * b) #multiply
-# print(a / b) #div
-# print(a % b) #remainder
-# print(a ** b) #a^b power
+print("the Arithmetic Operators are")
+print(a + b) #addition
+print(a - b) #sub
+print(a * b) #multiply
+print(a / b) #div
+print(a % b) #remainder
+print(a ** b) #a^b power
 
 #relational operators
 a = 30
 b = 50
 
-# print(a == b) #False
-# print(a != b) #True 
-# print(a >= b) #Flase
-# print(a > b) #False
-# print(a <= b) #True
-# print(a < b) #True
+print(a == b) #False
+print(a != b) #True 
+print(a >= b) #Flase
+print(a > b) #False
+print(a <= b) #True
+print(a < b) #True
 
 #assignment operators
-# num = 20
-# num += 10    #num = num + 10
-# num -= 5
-# num *= 4
-# num /= 10
-# num %= 7   #gives remainder
-# num **= 3   #power
-#print("num : " , num)
+num = 20
+num += 10    #num = num + 10
+num -= 5
+num *= 4
+num /= 10
+num %= 7   #gives remainder
+num **= 3   #power
+print("num : " , num)
 
 #logical operators
 a = 30
 b = 20
 
-# print(a > b)
-# print(a < b)
-# print(not (a > b))
-# print(not (a < b))
+print(a > b)
+print(a < b)
+print(not (a > b))
+print(not (a < b))
 
 val1 = True 
 val2 = False 
-# print("AND operator:" , val1 and val2)
+print("AND operator:" , val1 and val2)
 
-# print("OR operator:" , val1 or val2) 
+print("OR operator:" , val1 or val2) 
 
-# print("AND operator:" , (a != b) and (a > b)) 
+print("AND operator:" , (a != b) and (a > b)) 
 
 print("BE CONSISTENT !!!")
 print("KEEP IT IP :)") 
+
 
 
 
