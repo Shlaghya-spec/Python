d8966f605fced581784a99415a6dbab0c201f436
Shlaghya Bhosale <shlaghyabhosale@gmail.com>
2025-09-20 16:58:10 +0530

    CH-5 loops 1.0

--- loops2.py
+++ loops2.py
@@ -1,58 +1,58 @@
 print("DAY 15") # DATE : 20-09-2025  
 #LOOPS
 
-# count = 1
-# while count <= 5 :
-#     print("consistent")
-#     count += 1
-# print("the count is : " , count) 
+count = 1
+while count <= 5 :
+    print("consistent")
+    count += 1
+print("the count is : " , count) 
 
-# i = 1
-# while i <= 38: 
-#     print("kashmir" , i)
-#     i += 1
-# print("the count is : " , i) 
+i = 1
+while i <= 38: 
+    print("kashmir" , i)
+    i += 1
+print("the count is : " , i) 
 
 # print 1 to 10
 
-# i = 1
-# while i <= 10:
-#     print(i)
-#     i += 1
+i = 1
+while i <= 10:
+    print(i)
+    i += 1
 
-# print("Loop Ended")
+print("Loop Ended")
 
-# i = 5
-# while i >= 1:
-#     print(i)
-#     i -= 1
+i = 5
+while i >= 1:
+    print(i)
+    i -= 1
 
-# print("reverse loop end")
+print("reverse loop end")
 
-# j = 5
-# while j >= 1:
-#     print("japan" , j)
-#     j -= 1
+j = 5
+while j >= 1:
+    print("japan" , j)
+    j -= 1
 
-# print("reverse string loop end")
+print("reverse string loop end")
 
 #practice time
 #print even nos
 
-# i = 2 #initialization
-# while i <= 30: #condition
-#     print(i)
-#     i += 2     #updation
-# print("loop end") 
+i = 2 #initialization
+while i <= 30: #condition
+    print(i)
+    i += 2     #updation
+print("loop end") 
 
 #sum of 5 natural no
 
-# j = 1
-# total = 0  
-# while j <= 5:
-#     total += j 
-#     j += 1
-# print("sum is :" , total) #15         
+j = 1
+total = 0  
+while j <= 5:
+    total += j 
+    j += 1
+print("sum is :" , total) #15         
 
 #qs print the multiplication table of any no. n
 
@@ -68,5 +68,6 @@ print("end")
 
     
     
+
 
 
